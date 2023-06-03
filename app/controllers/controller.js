a27497d556d7eee67f993256bb14d7892a24bde6
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-06-03 20:59:37 +0700

    update'

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -33,11 +33,11 @@ exports.signup = (req, res) => {
         errors.push("Failed to register user");
       } else {
         console.log("Sign up");
-        const currentDate = new Date()
+        const currentDate = new Date();
         bcrypt.hash(password, 10, (error, hash) => {
           connection.query(
             "INSERT INTO user (username, name, email, password, role, date_created, image) VALUES (?, ?, ?, ?, 1, ?, 'default.jpg')",
-            [username, username, email, hash, currentDate],
+            [username, username, email, hash, currentDate]
           );
         });
         // response success
@@ -53,46 +53,80 @@ exports.signup = (req, res) => {
   );
 };
 
-
-
 exports.login = (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    connection.query(
-      "SELECT password FROM user WHERE email = ?",
-      [email],
-      (error, results) => {
-        if (error) {
-          console.error("Error executing database query: " + error.stack);
-          res.status(500).json({ error: "An error occurred" });
-          return;
-        }
-
-        if (results.length > 0) {
-          const hashedPassword = results[0].password;
-          const idUser = results[0].id;
-
-          // Compare the entered password with the stored hashed password
-          bcrypt.compare(password, hashedPassword, (err, isMatch) => {
-            if (err) {
-              console.error("Error comparing passwords: " + err);
-              res.status(500).json({ error: "An error occurred" });
-              return;
-            }
-
-            if (isMatch) {
-              // Login successful
-              res.status(200).json({ message: "Login successful", idUser : idUser, email : email });
-            } else {
-              // Login failed
-              res.status(401).json({ error: "Invalid credentials" });
-            }
-          });
-        } else {
-          // Login failed (no user found)
-          res.status(401).json({ error: "Invalid credentials" });
-        }
+  connection.query(
+    "SELECT password FROM user WHERE email = ?",
+    [email],
+    (error, results) => {
+      if (error) {
+        console.error("Error executing database query: " + error.stack);
+        res.status(500).json({ error: "An error occurred" });
+        return;
       }
-    );
-  };
 
+      if (results.length > 0) {
+        const hashedPassword = results[0].password;
+        const idUser = results[0].id;
+
+        // Compare the entered password with the stored hashed password
+        bcrypt.compare(password, hashedPassword, (err, isMatch) => {
+          if (err) {
+            console.error("Error comparing passwords: " + err);
+            res.status(500).json({ error: "An error occurred" });
+            return;
+          }
+
+          if (isMatch) {
+            // Login successful
+            res.status(200).json({
+              message: "Login successful",
+              idUser: idUser,
+              email: email,
+            });
+          } else {
+            // Login failed
+            res.status(401).json({ error: "Invalid credentials" });
+          }
+        });
+      } else {
+        // Login failed (no user found)
+        res.status(401).json({ error: "Invalid credentials" });
+      }
+    }
+  );
+};
+
+exports.getProfile = (req, res) => {
+  const email = req.params.email;
+
+  const query = `SELECT * FROM user WHERE email = ?`;
+
+  connection.query(query, [email], (err, results) => {
+    if (err) {
+      console.error("Error retrieving user data:", err);
+      res.status(500).json({ error: "Failed to retrieve user data" });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ message: "User not found" });
+      } else {
+        res.json(results[0]);
+      }
+    }
+  });
+};
+
+exports.getFeeds = (req, res) => {
+  const sql = "SELECT * FROM feeds";
+  // Execute the query
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error("Error retrieving feed data:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    // Return the retrieved data as a JSON response
+    res.status(200).json(results);
+  });
+};
--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const {signup, login} = require("./app/controllers/controller.js");
+const {
+  signup,
+  login,
+  getProfile,
+  getFeeds,
+} = require("./app/controllers/controller.js");
+const axios = require("axios");
 
 const configuration = new Configuration({
   organization: "org-u58nSOXtYQRjzYr7RTCzqKpn",
-  apiKey: "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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
@@ -22,7 +28,53 @@ app.get("/", (req, res) => {
 
 app.post("/user", signup);
 app.post("/login", login);
+app.get("/profile/:email", getProfile);
+app.get("/feeds", getFeeds);
+
+app.post("/chat", async (req, res) => {
+  const { message } = req.body;
+  console.log(message);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${message}`,
+    max_tokens: 1000,
+    temperature: 0.5,
+  });
+
+  //   console.log(response.data.choices[0].text);
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+
+app.get("/check-token", async (req, res) => {
+  const testPrompt = "Hello, world!"; // Test prompt to send to the OpenAI API
+  const openaiApiKey = "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"; // Replace with your OpenAI API token
+
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/engines/davinci-codex/completions",
+      {
+        prompt: testPrompt,
+        max_tokens: 5,
+      },
+      {
+        headers: {
+          Authorization: `${openaiApiKey}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // If the response is successful and there are no errors, the token is valid
+    res.status(200).json({ valid: true });
+  } catch (error) {
+    console.log(error);
+    // If there is an error or authentication issue, the token is invalid
+    res.status(401).json({ valid: false });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
