16e4b823a7565314a962827354994735dc02b1fc
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-06-11 16:39:31 +0700

    update

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const connection = require("../models/db.js");
+const req = require("express/lib/request.js");
 
 exports.signup = (req, res) => {
   console.log("Empty input value check");
@@ -116,6 +117,31 @@ exports.getProfile = (req, res) => {
     }
   });
 };
+exports.getProfileImage = (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT image FROM user WHERE id = ?`;
+
+  // execute query
+  connection.query(query, [id], (err, results) => {
+    if (err) {
+      console.error("Error retrieving user data:", err);
+      res.status(500).json({ error: "Failed to retrieve user data" });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ message: "User not found" });
+      } else {
+        console.log(results[0].image);
+        // res.sendFile(__dirname + `./../../images/${results[0].image}`);
+        // response by sending image
+        // forbidden error
+        console.log("Request comming");
+        res.sendFile(__dirname + "/images/" + results[0].image);
+        // res.sendFile(__dirname + `./../../images/default.jpg`);
+        // res.sendFile(__dirname + `./../../images/not mirror.jpeg`);
+      }
+    }
+  });
+};
 
 exports.getFeeds = (req, res) => {
   const sql = "SELECT * FROM feeds";
@@ -130,3 +156,195 @@ exports.getFeeds = (req, res) => {
     res.status(200).json(results);
   });
 };
+
+exports.getChatLog = (req, res) => {
+  const userId = req.params.userId;
+
+  const query = `SELECT * FROM chat_log WHERE user_id = ? order by id desc`;
+
+  // Execute the query
+  connection.query(query, [userId], (err, results) => {
+    if (err) {
+      console.error("Error retrieving feed data:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    // Return the retrieved data as a JSON response
+    res.status(200).json(results);
+  });
+};
+
+exports.editProfile = (req, res) => {
+  const { id } = req.params;
+  const { username, name, email } = req.body;
+
+  const query = `UPDATE user SET username = ?, name = ?, email = ? WHERE id = ?`;
+
+  // Execute the query
+  connection.query(query, [username, name, email, id], (err, results) => {
+    if (err) {
+      console.error("Error retrieving feed data:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    // Return the retrieved data as a JSON response
+    res.status(200).json(results);
+  });
+};
+
+exports.editPassword = (req, res) => {
+  const { id } = req.params;
+  const { oldPassword, password } = req.body;
+  console.log(oldPassword, password);
+  const query = `SELECT password FROM user WHERE id = ?`;
+  // compare old password with the stored hashed password
+  connection.query(query, [id], (err, results) => {
+    if (err) {
+      console.error("Error retrieving feed data:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    const hashedPassword = results[0].password;
+    bcrypt.compare(oldPassword, hashedPassword, (err, isMatch) => {
+      if (err) {
+        console.error("Error comparing passwords: " + err);
+        res.status(500).json({ error: "An error occurred" });
+        return;
+      }
+      // if same update password with new password that already hashed
+      if (isMatch) {
+        bcrypt.hash(password, 10, (err, hash) => {
+          if (err) {
+            console.error("Error hashing password: " + err);
+            res.status(500).json({ error: "An error occurred" });
+            return;
+          }
+          const query = `UPDATE user SET password = ? WHERE id = ?`;
+          connection.query(query, [hash, id], (err, results) => {
+            if (err) {
+              console.error("Error retrieving feed data:", err);
+              res.status(500).json({ error: "Internal Server Error" });
+              return;
+            }
+            // Return the retrieved data as a JSON response
+            res.status(200).json(results);
+          });
+        });
+      } else {
+        res.status(401).json({ error: "Invalid credentials" });
+      }
+    });
+  });
+};
+
+exports.addClass = (req, res) => {
+  const { name, code, password, description, lecture_id } = req.body;
+  console.log(name, code, password, description);
+  const query = `INSERT INTO kelas (class_name, class_code, password, description, lecture_id, image) VALUES (?, ?, ?, ?, ?, 'class_default.jpg')`;
+  connection.query(
+    query,
+    [name, code, password, description, lecture_id],
+    (err, results) => {
+      if (err) {
+        console.error("Error retrieving feed data:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+      // Return the retrieved data as a JSON response
+      res.status(200).json(results);
+    }
+  );
+};
+
+exports.getAllUsers = (req, res) => {
+  // get all data from table user
+  const query = `SELECT * FROM user`;
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error retrieving user data:", err);
+      res.status(500).json({ error: "Failed to retrieve user data" });
+    } else {
+      res.json(results);
+    }
+  });
+};
+
+
+exports.getAllClasses = (req, res) => {
+  // get all data from table user
+  const query = `SELECT * FROM kelas`;
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error retrieving user data:", err);
+      res.status(500).json({ error: "Failed to retrieve user data" });
+    } else {
+      // image field return as a blob
+      // res send file
+
+      res.json(results);
+    }
+  });
+}
+
+
+// get all classes image
+exports.getClassImage = (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  const query = `SELECT image FROM kelas WHERE class_code = ?`;
+  connection.query(query, [id], (err, results) => {
+    if (err) {
+      console.error("Error retrieving user data:", err);
+      res.status(500).json({ error: "Failed to retrieve user data" });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ message: "User not found" });
+      } else {
+        console.log(results[0].image);
+        // res.sendFile(__dirname + `./../../images/${results[0].image}`);
+        // response by sending image
+        // forbidden error
+        console.log("Request comming");
+        res.sendFile(__dirname + "/images/" + results[0].image);
+        // res.sendFile(__dirname + `./../../images/default.jpg`);
+        // res.sendFile(__dirname + `./../../images/not mirror.jpeg`);
+      }
+    }
+  });
+}
+
+
+exports.getSpecifiedClass = (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT * FROM kelas WHERE class_code = ?`;
+  connection.query(query, [id], (err, results) => {
+    if (err) {
+      console.error("Error retrieving user data:", err);
+      res.status(500).json({ error: "Failed to retrieve user data" });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ message: "User not found" });
+      } else {
+        res.json(results[0]);
+      }
+    }
+  });
+}
+
+exports.getSpecifiedUser = (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const query = `SELECT * FROM user WHERE id = ?`;
+  connection.query(query, [id], (err, results) => {
+    if (err) {
+      console.error("Error retrieving user data:", err);
+      res.status(500).json({ error: "Failed to retrieve user data" });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ message: "User not found" });
+      } else {
+        res.json(results[0]);
+      }
+    }
+  });
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
+const connection = require("./app/models/db.js");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -7,16 +8,39 @@ const {
   login,
   getProfile,
   getFeeds,
+  getChatLog,
+  getProfileImage,
+  editProfile,
+  editPassword,
+  addClass,
+  getAllUsers,
+  getAllClasses,
+  getClassImage,
+  getSpecifiedClass,
+  getSpecifiedUser
 } = require("./app/controllers/controller.js");
 const axios = require("axios");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "app/controllers/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const configuration = new Configuration({
   organization: "org-u58nSOXtYQRjzYr7RTCzqKpn",
-  apiKey: "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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -26,21 +50,90 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.post("/image/:id", upload.single("file"), function (req, res) {
+  // get file name
+  const { file } = req;
+  console.log(file);
+  $query = "UPDATE user SET image = ? WHERE id = ?";
+  connection.query(
+    $query,
+    [file.originalname, req.params.id],
+    (err, result) => {
+      if (err) {
+        console.error("Error inserting data:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+      res.status(200).json({ message: "Image uploaded successfully" });
+    }
+  );
+});
+
+app.get("/image_profile/:id", getProfileImage);
+
 app.post("/user", signup);
 app.post("/login", login);
+app.put("/profile/:id", editProfile);
+app.put("/edit_password/:id", editPassword);
 app.get("/profile/:email", getProfile);
+app.get("/chatlog/:userId", getChatLog);
 app.get("/feeds", getFeeds);
+app.post("/class", addClass);
+app.get("/all_users", getAllUsers);
+app.get("/all_classes", getAllClasses);
+app.get("/class_image/:id", getClassImage);
+app.get("/class/:id", getSpecifiedClass);
+app.get("/user/:id", getSpecifiedUser);
 
 app.post("/chat", async (req, res) => {
-  const { message } = req.body;
-  console.log(message);
+  const { message, user_id, date } = req.body;
+  console.log(message, user_id, date);
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `${message}`,
     max_tokens: 1000,
     temperature: 0.5,
   });
+  const sql =
+    "INSERT INTO chat_log (user_id, message, date, is_from_user) VALUES (?, ?, ?, 1)";
 
+  // Execute the query with the provided data
+  connection.query(sql, [user_id, message, date], (err, result) => {
+    if (err) {
+      console.error("Error inserting data:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+  });
+
+  const sqlResponse =
+    "INSERT INTO chat_log (user_id, message, date, is_from_user) VALUES (?, ?, ?, 0)";
+  connection.query(
+    sqlResponse,
+    [user_id, response.data.choices[0].text, date],
+    (err, result) => {
+      if (err) {
+        console.error("Error inserting data:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+    }
+  );
+
+  //   console.log(response.data.choices[0].text);
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+app.post("/resume", async (req, res) => {
+  const { book } = req.body;
+  console.log(book);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Bisakah kamu buatkan summary secara lengkap singkat dan padat terkait buku ${book} minimal 500 kata !`,
+    max_tokens: 2000,
+    temperature: 0.5,
+  });
   //   console.log(response.data.choices[0].text);
   res.json({
     message: response.data.choices[0].text,
